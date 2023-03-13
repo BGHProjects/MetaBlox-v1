b438dca557678228bbc56408c58c343c7f885ff1
BGHProjects <un12known34_acc@outlook.com>
2023-03-13 21:21:04 +1000

    feat: :fire: fixed issues with Light

--- next-app/components/Menu/MenuBackground/Light.tsx
+++ next-app/components/Menu/MenuBackground/Light.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from "react";
+import { Vector3 } from "three";
 import { gridBlue, gridPink } from "../../../constants/colours";
 import useColourAnimation from "../../../hooks/useColorAnimation";
 
@@ -7,16 +8,6 @@ import useColourAnimation from "../../../hooks/useColorAnimation";
  * in the Menu background
  */
 const Light = () => {
-  const spotlight1Ref = useRef();
-  const spotlight2Ref = useRef();
-  const spotlight3Ref = useRef();
-  const spotlight4Ref = useRef();
-
-  (spotlight1Ref.current as any)!.target.position.set([-0.25, 0.25, 0.25]);
-  (spotlight2Ref.current as any)!.target.position.set([0.25, 0.25, 0.25]);
-  (spotlight3Ref.current as any)!.target.position.set([-0.25, 0.25, 0.25]);
-  (spotlight4Ref.current as any)!.target.position.set([0.25, 0.25, 0.25]);
-
   const [lightColor, setLightColour] = useState("");
 
   const color = useColourAnimation(gridBlue, gridPink);
@@ -25,48 +16,26 @@ const Light = () => {
     setLightColour(color);
   }, [color]);
 
+  const positions = [
+    [0.5, 0.75, 2.1],
+    [-0.5, 0.75, 2.1],
+    [0, -0.75, 3],
+    [0, -0.75, 3],
+  ];
+
   return (
     <>
-      <spotLight
-        ref={spotlight1Ref as any}
-        color={lightColor}
-        intensity={40}
-        position={[0.5, 0.75, 2.1]}
-        distance={25}
-        angle={Math.PI * 0.1}
-        penumbra={0.25}
-        decay={10}
-      />
-      <spotLight
-        ref={spotlight2Ref as any}
-        color={lightColor}
-        intensity={40}
-        position={[-0.5, 0.75, 2.1]}
-        distance={25}
-        angle={Math.PI * 0.1}
-        penumbra={0.25}
-        decay={0}
-      />
-      <spotLight
-        ref={spotlight3Ref as any}
-        color={lightColor}
-        intensity={40}
-        position={[0, -0.75, 3]}
-        distance={25}
-        angle={-Math.PI * 0.1}
-        penumbra={0.25}
-        decay={0}
-      />
-      <spotLight
-        ref={spotlight4Ref as any}
-        color={lightColor}
-        intensity={40}
-        position={[0, -0.75, 3]}
-        distance={25}
-        angle={-Math.PI * 0.1}
-        penumbra={0.25}
-        decay={0}
-      />
+      {positions.map((position) => (
+        <spotLight
+          color={lightColor}
+          intensity={40}
+          position={position as any}
+          distance={25}
+          angle={Math.PI * 0.1}
+          penumbra={0.25}
+          decay={10}
+        />
+      ))}
     </>
   );
 };
